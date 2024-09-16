3e5ad9168fa50b7ee8d76da50df0451fb1297048
Corentin Paulet <corentin.paulet@getyooz.com>
2024-09-16 14:22:11 +0200

    connexion user done, logout done, jwt-decode & @auth0/angular-jwt installed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,9 +10,6 @@ import {LoginGuard} from './login.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'connexion', pathMatch: 'full' },
-  /*fair une if utilisateur non connecté alors redirect to connexion, else rediriger vers home
-  * si utilisateur non connecté va directement sur ica.com/home, redirigier vers ica.com/connexion
-  * pareil pour l'invrse si utilisater deja connecté mais va sur ica.com/connexion, rediriger vers ica.com/home */
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'connexion', component: ConnexionComponent, canActivate: [LoginGuard] },
   { path: 'new-hook', component: HookFormComponent, canActivate: [AuthGuard]  },
